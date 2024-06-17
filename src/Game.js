19e200217bb88621bd78b51942ba2066f5a820f4
Larry Harmon <harmonwebdesign@gmail.com>
2024-06-16 22:31:20 -0400

    add save game state and reduce

--- src/Game.js
+++ src/Game.js
@@ -1,22 +1,45 @@
-import React, { useState, useEffect, useRef, forwardRef, useImperativeHandle } from "react";
+import React, { useState, useEffect, useRef, forwardRef, useImperativeHandle, useCallback, useReducer } from "react";
 import StatusBar from "./components/StatusBar";
 import { notify } from "./notification";
 import Keyboard from "./components/Keyboard";
 import { validWords } from "./validWords";
 import { possibleAnswers } from "./possibleAnswers";
 import { getStats, updateStats } from "./utils/stats";
+import Cookies from "js-cookie";
+
+import { MdMenuBook } from "react-icons/md";
+
+const lettersReducer = (state, action) => {
+  switch (action.type) {
+    case "addCorrect":
+      return { ...state, correct: [...state.correct, action.payload] };
+    case "addIncorrect":
+      return { ...state, incorrect: [...state.incorrect, action.payload] };
+    case "addNotInWord":
+      return { ...state, notInWord: [...state.notInWord, action.payload] };
+    case "RESET":
+      return { correct: [], incorrect: [], notInWord: [] };
+    case "RESTORE_STATE":
+      return action.payload;
+    default:
+      throw new Error();
+  }
+};
+
+// Function to create an empty grid
+const createEmptyGrid = () =>
+  Array(6)
+    .fill("")
+    .map(() => Array(5).fill({ letter: "", color: "" }));
 
 const Game = forwardRef(({ setStats, setIsModalOpen, setGameStatus, isHardMode, gameStatus }, ref) => {
-  const [grid, setGrid] = useState(
-    Array(6)
-      .fill("")
-      .map(() => Array(5).fill({ letter: "", color: "" }))
-  );
+  const [usedLetters, dispatch] = useReducer(lettersReducer, { correct: [], incorrect: [], notInWord: [] });
+  const [grid, setGrid] = useState(createEmptyGrid());
   const [currentRow, setCurrentRow] = useState(0);
   const [currentCol, setCurrentCol] = useState(0);
   const [currentWord, setCurrentWord] = useState("");
   const [solution, setSolution] = useState("");
-  const [usedLetters, setUsedLetters] = useState({ correct: [], incorrect: [], notInWord: [] });
+
   const [animatingCells, setAnimatingCells] = useState([]); // State to track animating cells
   const gridRef = useRef(null);
 
@@ -24,28 +47,47 @@ const Game = forwardRef(({ setStats, setIsModalOpen, setGameStatus, isHardMode, 
     resetGame,
   }));
 
-  const resetGame = () => {
-    setGrid(
-      Array(6)
-        .fill("")
-        .map(() => Array(5).fill({ letter: "", color: "" }))
-    );
+  const resetGame = useCallback(() => {
+    setGrid(createEmptyGrid);
     setCurrentRow(0);
     setCurrentCol(0);
     setCurrentWord("");
     setGameStatus("");
-    setUsedLetters({ correct: [], incorrect: [], notInWord: [] }); // Reset usedLetters
+    dispatch({ type: "RESET" }); // Reset usedLetters
     setSolution(possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)]);
     gridRef.current.focus();
-  };
+  }, [dispatch, gridRef, setGameStatus]); //Add dependencies if any variables used inside resetGame change over time
 
   useEffect(() => {
-    setSolution(possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)]);
-  }, []);
+    const setNewSolution = () => {
+      setSolution(possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)]);
+    };
+
+    const restoreGameState = (state) => {
+      setSolution(state.solution);
+      setGrid(state.grid);
+      setCurrentRow(state.currentRow);
+      setCurrentCol(state.currentCol);
+      setCurrentWord(state.currentWord);
+      setGameStatus(state.gameStatus);
+      dispatch({ type: "RESTORE_STATE", payload: state.usedLetters });
+    };
+
+    const savedState = Cookies.get("gameState");
+
+    if (savedState) {
+      const state = JSON.parse(savedState);
+      state.solution === "" ? setNewSolution() : restoreGameState(state);
+    } else {
+      setNewSolution();
+    }
+  }, [setGameStatus]);
 
   useEffect(() => {
-    gridRef.current.focus();
-  }, [solution]);
+    if (gridRef.current) {
+      gridRef.current.focus();
+    }
+  }, [solution, grid, currentRow, currentCol, currentWord, usedLetters, animatingCells]);
 
   //validation for hard mode
   const validateHardMode = (word) => {
@@ -108,7 +150,7 @@ const Game = forwardRef(({ setStats, setIsModalOpen, setGameStatus, isHardMode, 
 
   const animateRow = (rowIndex, word) => {
     const updatedGrid = [...grid];
-    const newUsedLetters = { ...usedLetters };
+    // const newUsedLetters = { ...usedLetters };
     let isCorrect = true;
 
     const animateCell = (colIndex) => {
@@ -118,14 +160,16 @@ const Game = forwardRef(({ setStats, setIsModalOpen, setGameStatus, isHardMode, 
         setTimeout(() => {
           if (letter === solution[colIndex]) {
             updatedGrid[rowIndex][colIndex].color = "bg-customGreen text-white";
-            newUsedLetters.correct.push(letter);
+            dispatch({ type: "addCorrect", payload: letter });
           } else if (solution.includes(letter)) {
             updatedGrid[rowIndex][colIndex].color = "bg-customBlue text-white";
-            newUsedLetters.incorrect.push(letter);
+
+            dispatch({ type: "addIncorrect", payload: letter });
             isCorrect = false;
           } else {
             updatedGrid[rowIndex][colIndex].color = "bg-zinc-500 text-white";
-            newUsedLetters.notInWord.push(letter);
+
+            dispatch({ type: "addNotInWord", payload: letter });
             isCorrect = false;
           }
           setAnimatingCells((prev) => prev.filter((cell) => !(cell.row === rowIndex && cell.col === colIndex)));
@@ -136,23 +180,26 @@ const Game = forwardRef(({ setStats, setIsModalOpen, setGameStatus, isHardMode, 
           animateCell(colIndex + 1);
         }, 400); // Delay for the next cell animation
       } else {
-        setUsedLetters(newUsedLetters);
+        // setUsedLetters(newUsedLetters);
         if (isCorrect) {
           setCurrentRow((prevRow) => prevRow + 1); // Increment the row to reflect the winning attempt
           setGameStatus("You win!");
           updateStats(currentRow + 1, true); // Update the stats with the number of guesses
           setStats(getStats()); // Update the stats state
           setIsModalOpen(true); // Open the modal
+          saveGameState(currentRow + 1, "You win!");
         } else if (currentRow === 5) {
           setGameStatus(`You lost! The word was ${solution}.`);
           updateStats(currentRow + 1, false); // Update stats with loss status
           setStats(getStats()); // Refresh the stats state
           setIsModalOpen(true); // Open the modal
           setCurrentRow((prevRow) => prevRow + 1);
+          saveGameState(currentRow + 1, `You lost The word was ${solution}.`);
         } else {
           setCurrentRow((prevRow) => prevRow + 1);
           setCurrentCol(0);
           setCurrentWord("");
+          saveGameState(currentRow + 1, "");
         }
       }
     };
@@ -160,13 +207,31 @@ const Game = forwardRef(({ setStats, setIsModalOpen, setGameStatus, isHardMode, 
     animateCell(0);
   };
 
+  // Save state of game to cookies
+  const saveGameState = (row, game_Status) => {
+    const state = {
+      grid,
+      currentRow: row, // Save the current row for the next game
+      currentCol: 0, // Reset current column to 0 when saving state
+      currentWord: "",
+      gameStatus: game_Status,
+      usedLetters,
+      solution,
+    };
+    Cookies.set("gameState", JSON.stringify(state), { expires: 365 });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center bg-indigo-50">
       {gameStatus ? (
-        <div className="flex md:text-sm text-xs gap-2 pt-1">
-          <a href={`https://www.dictionary.com/browse/${solution}`} target="_blank" rel="noreferrer">
-            Answer: <span className="underline ml-1">{solution}</span>
+        <div className="flex md:text-sm text-xs gap-2 pt-1 ">
+          <a href={`https://www.dictionary.com/browse/${solution}`} className="flex items-center  align-middle no-wrap" target="_blank" rel="noreferrer">
+            Answer:{" "}
+            <span className="underline ml-1 flex items-center">
+              <MdMenuBook className="size-5 mx-2" style={{ marginTop: "-5" }} /> {solution}
+            </span>
           </a>
+
           <button onClick={() => resetGame()} className="md:text-sm text-xs text-customBlue">
             play again?
           </button>
